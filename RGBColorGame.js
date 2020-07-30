7cea9d34590552a758ff2f6e04d8247e16a2fcde
AlexandreATavares <aleatavares@hotmail.com>
2020-07-30 17:52:38 -0300

    Add logic to difficulty buttons

--- RGBColorGame.js
+++ RGBColorGame.js
@@ -11,11 +11,28 @@ var hardBtn = document.querySelector("#hardBtn");
 easyBtn.addEventListener("click", function () {
   hardBtn.classList.remove("selected");
   easyBtn.classList.add("selected");
+  colors = generateRandomColors(3);
+  pickedColor = pickColor();
+  colorDisplay.textContent = pickedColor;
+  for (var i = 0; i < squares.length; i++) {
+    if (colors[i]) {
+      squares[i].style.backgroundColor = colors[i];
+    } else {
+      squares[i].style.display = "none";
+    }
+  }
 });
 
 hardBtn.addEventListener("click", function () {
   easyBtn.classList.remove("selected");
   hardBtn.classList.add("selected");
+  colors = generateRandomColors(6);
+  pickedColor = pickColor();
+  colorDisplay.textContent = pickedColor;
+  for (var i = 0; i < squares.length; i++) {
+    squares[i].style.backgroundColor = colors[i];
+    squares[i].style.display = "block";
+  }
 });
 
 resetButton.addEventListener("click", function () {
