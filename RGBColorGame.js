1cb95a3ff9f550817b018cada3394d4ef5f0f111
AlexandreATavares <aleatavares@hotmail.com>
2020-07-29 21:17:19 -0300

    Add randomness to the colors to be guessed

--- RGBColorGame.js
+++ RGBColorGame.js
@@ -1,11 +1,4 @@
-var colors = [
-  "rgb(255, 0, 0)",
-  "rgb(255, 255, 0)",
-  "rgb(0, 255, 0)",
-  "rgb(0, 255, 255)",
-  "rgb(0, 0, 255)",
-  "rgb(255, 0, 255)",
-];
+var colors = generateRandomColors(6);
 
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
@@ -44,4 +37,26 @@ function changeColors(color) {
 function pickColor() {
   var random = Math.floor(Math.random() * colors.length);
   return colors[random];
+}
+
+function generateRandomColors(num) {
+  //make array
+  var arr = [];
+  //repeat num times
+  for (var i = 0; i < num; i++) {
+    //get random color and push into array
+    arr.push(randomColor());
+  }
+  //return that array
+  return arr;
+}
+
+function randomColor() {
+  //pick a red color from 0 to 255
+  var r = Math.floor(Math.random() * 256);
+  //pick a green color from 0 to 255
+  var g = Math.floor(Math.random() * 256);
+  //pick a blue color from 0 to 255
+  var b = Math.floor(Math.random() * 256);
+  return "rgb(" + r + ", " + g + ", " + b + ")";
 }
