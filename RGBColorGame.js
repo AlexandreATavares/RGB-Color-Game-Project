301a9f6ed2371f6a7772512a4901ed1bdab972ad
AlexandreATavares <aleatavares@hotmail.com>
2020-07-30 17:38:04 -0300

    Add buttons for Easy and Hard Mode

--- RGBColorGame.js
+++ RGBColorGame.js
@@ -5,6 +5,18 @@ var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
 var h1 = document.querySelector("h1");
 var resetButton = document.querySelector("#reset");
+var easyBtn = document.querySelector("#easyBtn");
+var hardBtn = document.querySelector("#hardBtn");
+
+easyBtn.addEventListener("click", function () {
+  hardBtn.classList.remove("selected");
+  easyBtn.classList.add("selected");
+});
+
+hardBtn.addEventListener("click", function () {
+  easyBtn.classList.remove("selected");
+  hardBtn.classList.add("selected");
+});
 
 resetButton.addEventListener("click", function () {
   //generate all new colors
